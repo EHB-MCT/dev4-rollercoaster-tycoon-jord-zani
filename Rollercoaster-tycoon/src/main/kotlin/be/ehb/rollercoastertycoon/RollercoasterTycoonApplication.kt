1877226d0c16568910b125aa7b43bac491bcc1d4
Zani Dobruna <zani.dobruna@student.ehb.be>
2024-05-22 17:23:49 +0200

    Backend

---
+++ Rollercoaster-tycoon/src/main/kotlin/be/ehb/rollercoastertycoon/RollercoasterTycoonApplication.kt
@@ -0,0 +1,11 @@
+package be.ehb.rollercoastertycoon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RollercoasterTycoonApplication
+
+fun main(args: Array<String>) {
+	runApplication<RollercoasterTycoonApplication>(*args)
+}
